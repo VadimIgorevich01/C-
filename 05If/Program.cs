f33771fc227eb6566491f6cfa3d3fa9b4d6c6ec2
Vadim <01prosto01@mail.ru>
2023-05-18 18:03:53 +1000

    05If added

---
+++ 05If/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("What is your name?");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "vadim")
+{
+    Console.WriteLine("Vadim!!! Nice to see you again");
+}
+else 
+{
+    Console.WriteLine("Hello, " + username);
+}
