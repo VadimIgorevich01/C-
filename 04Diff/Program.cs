d879c689b4b623975ffc59157d36e4d77c818ae6
Vadim <01prosto01@mail.ru>
2023-05-18 17:52:10 +1000

    Random 04Diff

--- 04Diff/Program.cs
+++ 04Diff/Program.cs
@@ -1,3 +1,5 @@
-﻿double numberA = 11;
-double numberB = 4;
+﻿double numberA = new Random().Next(1, 10); // случайное целое от 1 до 9
+Console.WriteLine(numberA);
+double numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
 Console.WriteLine(numberA / numberB);
